4a2a9fc41cf78b95b90e96321d0ba89dac5e094a
ene99167788 <eneylton@hotmail.com>
2021-01-03 18:31:07 -0300

    ionic-pagSeguro-2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { HttpClientModule } from '@angular/common/http';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { IonicStorageModule } from '@ionic/storage';
+import { DatePipe } from '@angular/common';
+import { VarGlobalProvider } from '../providers/var-global/var-global';
+import { PagSeguroProvider } from '../providers/pag-seguro/pag-seguro';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -27,8 +34,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   providers: [
     StatusBar,
+    DatePipe,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    VarGlobalProvider,
+    PagSeguroProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PagSeguroProvider } from '../../providers/pag-seguro/pag-seguro';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,8 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private pagseg: PagSeguroProvider) {
+    this.pagseg.iniciar("eneylton@hotmail.com", "A7BC0FCBD91A4571A741E367DD355A98", true);
 
   }
 
